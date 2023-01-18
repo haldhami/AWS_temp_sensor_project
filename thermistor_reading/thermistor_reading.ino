dbd30a2456ae5d08e4eabdb0b4cf32bbd916c615
randomrandomer <66445070+randomrandomer@users.noreply.github.com>
2023-01-18 20:25:05 +0000

    updated arduino file

--- thermistor_reading/thermistor_reading.ino
+++ thermistor_reading/thermistor_reading.ino
@@ -1,5 +1,5 @@
 // the value of the series resistor
-#define SERIESRESISTOR 10000
+// #define SERIESRESISTOR 10000
 // What pin to connect the first sensor to
 #define THERMISTORPIN_IN A0
 // Define pin for second thermistor
@@ -9,12 +9,13 @@
 // temp. for nominal resistance (almost always 25 C)
 #define TEMPERATURENOMINAL 25
 // The beta coefficient of the thermistor (taken from spec sheet)
-#define BCOEFFICIENT 4100
+#define BCOEFFICIENT1 4150
+#define BCOEFFICIENT2 4100
 // The number of samples to average over (max is 255 for memory reasons)
-#define NUMSAMPLES 255
+#define NUMSAMPLES 200
 
-float r_in[NUMSAMPLES];
-float r_out[NUMSAMPLES];
+float r[NUMSAMPLES][2];
+// float r_out[NUMSAMPLES];
 
 void setup(void) {
   Serial.begin(9600);
@@ -22,53 +23,53 @@ void setup(void) {
 
 void loop(void) {
   uint8_t i;
-  float avg_r_in;
-  float avg_r_out;
-
-  // take N samples in a row, with a slight delay
-  for (i = 0; i < NUMSAMPLES; i++) {
-    r_in[i] = analogRead(THERMISTORPIN_IN);
-    r_out[i] = analogRead(THERMISTORPIN_OUT);
-    delay(10);
-  }
-
-  // average all the samples out
-  avg_r_in = 0;
-  avg_r_out = 0;
-  for (i = 0; i < NUMSAMPLES; i++) {
-    avg_r_in += r_in[i];
-    avg_r_out += r_out[i];
-  }
-  avg_r_in /= NUMSAMPLES;
-  avg_r_out /= NUMSAMPLES;
-
-  // convert the value to resistance
-  avg_r_in = (1023 / avg_r_in) - 1;      // (1023/ADC - 1)
-  avg_r_in = SERIESRESISTOR / avg_r_in;  // 10K / (1023/ADC - 1)
-
-  avg_r_out = (1023 / avg_r_out) - 1;
-  avg_r_out = SERIESRESISTOR / avg_r_out;
-
   float temp_in;
   float temp_out;
 
-  temp_in = avg_r_in / THERMISTORNOMINAL;           // (R/Ro)
+  // take N samples in a row, over the course of a minute
+  for (i = 0; i < NUMSAMPLES; i++) {
+    r[i][0] = analogRead(THERMISTORPIN_IN);
+    r[i][1] = analogRead(THERMISTORPIN_OUT);
+    delay(300);
+  }
+  
+
+  // average all the samples out
+  temp_in = 0;
+  temp_out = 0;
+  for (i = 0; i < NUMSAMPLES; i++) {
+    temp_in += r[i][0];
+    temp_out += r[i][1];
+  }
+  temp_in /= NUMSAMPLES;
+  temp_out /= NUMSAMPLES;
+
+  // convert the value to resistance
+  temp_in = (1023 / temp_in) - 1;      // (1023/ADC - 1)
+  temp_in = THERMISTORNOMINAL / temp_in;  // 10K / (1023/ADC - 1)
+
+  temp_out = (1023 / temp_out) - 1;
+  temp_out = THERMISTORNOMINAL / temp_out;
+
+
+
+  temp_in = temp_in / THERMISTORNOMINAL;           // (R/Ro)
   temp_in = log(temp_in);                        // ln(R/Ro)
-  temp_in /= BCOEFFICIENT;                         // 1/B * ln(R/Ro)
+  temp_in /= BCOEFFICIENT1;                         // 1/B * ln(R/Ro)
   temp_in += 1.0 / (TEMPERATURENOMINAL + 273.15);  // + (1/To)
   temp_in = 1.0 / temp_in;                       // Invert
   temp_in -= 273.15;                               // convert absolute temp to C
 
-  temp_out = avg_r_out / THERMISTORNOMINAL;
+  temp_out = temp_out / THERMISTORNOMINAL;
   temp_out = log(temp_out);
-  temp_out /= BCOEFFICIENT;
+  temp_out /= BCOEFFICIENT2;
   temp_out += 1.0 / (TEMPERATURENOMINAL + 273.15);
   temp_out = 1.0 / temp_out;
   temp_out -= 273.15;
 
   Serial.print(temp_in);
-  Serial.print(',')
-  Serial.println(temp_out)
+  Serial.print(F(","));
+  Serial.println(temp_out);
 
-  delay(5000);
+  // delay(5000);
 }
